ce0e49707b01f3e05f5e56ea059f662ad178a7d9
CXwudi <cx2010017@gmail.com>
2020-12-17 23:42:31 -0500

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vocadb-video-downloader-new
-A video downloader for VocaDB, download a list of Vocaloid PVs and extract them into music files
---
+++ vvd-downloader/src/main/java/mikufan/cx/vvd/downloader/DownloaderApplication.java
@@ -0,0 +1,15 @@
+package mikufan.cx.vvd.downloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Charles Chen 101035684
+ * @date 2020-12-17
+ */
+@SpringBootApplication
+public class DownloaderApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(DownloaderApplication.class, args);
+  }
+}
