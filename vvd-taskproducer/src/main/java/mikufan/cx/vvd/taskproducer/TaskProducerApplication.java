33445e942ef6ec2efa9206c8c966af2a5832ed7d
CXwudi <cx2010017@gmail.com>
2021-02-12 22:45:26 -0500

    :bento: spring init

--- vvd-taskproducer/src/main/java/mikufan/cx/vvd/taskproducer/TaskProducerApplication.java
+++ vvd-taskproducer/src/main/java/mikufan/cx/vvd/taskproducer/TaskProducerApplication.java
@@ -1,8 +1,15 @@
 package mikufan.cx.vvd.taskproducer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author CX无敌
  * @date 2021-02-12
  */
+@SpringBootApplication
 public class TaskProducerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TaskProducerApplication.class, args);
+  }
 }
