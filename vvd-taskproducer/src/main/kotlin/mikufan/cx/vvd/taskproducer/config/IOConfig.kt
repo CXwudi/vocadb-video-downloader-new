04765f87cc151004d1ed53f7fd768fcab75a7b28
CXwudi <cx2010017@gmail.com>
2021-06-18 21:10:00 -0400

    update doc

--- vvd-taskproducer/src/main/kotlin/mikufan/cx/vvd/taskproducer/config/IOConfig.kt
+++ vvd-taskproducer/src/main/kotlin/mikufan/cx/vvd/taskproducer/config/IOConfig.kt
@@ -17,7 +17,7 @@ import java.nio.file.Path
 @PathsNotSame(fields = ["outputDirectory", "errorDirectory"])
 data class IOConfig(
   val inputListId: Int,
-  // in Kotlin, annotations here by default are constructor's parameters.
+  // in Kotlin, annotations here by default are constructor's parameters. https://kotlinlang.org/docs/annotations.html#annotation-use-site-targets
   // however, spring doesn't support constructor validation. https://www.baeldung.com/javax-validation-method-constraints#1-automatic-validation-with-spring
   // so we need to make it either field or getter annotations by @field:Annotation or @get:Annotation
   // if @get:Annotation, spring will still evaluate it before application is up
