c7db3cd54568543a02dc48db6867db85f638bcbd
CXwudi <cx2010017@gmail.com>
2021-08-29 23:40:04 -0400

    :sparkles: first commit from new PC

--- vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/config/Enablement.kt
+++ vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/config/Enablement.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.validation.annotation.Validated
 
-
 /**
  * The config data class holding all enabled PV downloaders
  * @date 2021-06-20
@@ -19,7 +18,8 @@ import org.springframework.validation.annotation.Validated
 data class Enablement(
   val enablement: Map<PVServicesEnum, List<String>>
 ) { // can not use Class : Map<PVServices.Constant, List<String>> by enablement, throw NoSuchMethodException,
-  // consider to submit issue
+  // guess that is caused by Spring recognizing it as a Map<>
+  // consider submitting issue
   operator fun get(key: PVServicesEnum): List<String> = enablement[key] ?: emptyList()
   fun containsPvService(key: PVServicesEnum): Boolean = enablement.containsKey(key)
 }
