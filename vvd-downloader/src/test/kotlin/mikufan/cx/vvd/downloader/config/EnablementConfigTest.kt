3079a79a2e92260e79b7ccb09042f6a14aab798a
CXwudi <cx2010017@gmail.com>
2021-08-30 00:33:27 -0400

    remove unused test

--- vvd-downloader/src/test/kotlin/mikufan/cx/vvd/downloader/config/EnablementConfigTest.kt
+++ vvd-downloader/src/test/kotlin/mikufan/cx/vvd/downloader/config/EnablementConfigTest.kt
@@ -5,36 +5,12 @@ import mikufan.cx.inlinelogging.KInlineLogging
 import mikufan.cx.vvd.downloader.util.PVServicesEnum
 import mikufan.cx.vvd.downloader.util.SpringBootTestWithTestProfile
 import mikufan.cx.vvd.downloader.util.SpringShouldSpec
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.containsInAnyOrder
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
 /**
  * @date 2021-06-20
  * @author CX无敌
  */
-@SpringBootTestWithTestProfile(
-  customProperties = [
-    "config.preference.pv-preference=NicoNicoDouga, Youtube",
-    "config.enablement.NicoNicoDouga=youtube-dl, other-downloader",
-    "config.downloader.NicoNicoDouga.other-downloader.launch-cmd=./command,--some-args",
-  ]
-)
-internal class EnablementConfigTest(
-  @Autowired val enablement: Enablement
-) {
-
-  @Test
-  fun `should correctly enable`() {
-    log.debug { "enablement = $enablement" }
-    // should see that only youtube's and niconico's config existence are checked
-    assertThat(enablement[PVServicesEnum.NICONICODOUGA], containsInAnyOrder("youtube-dl", "other-downloader"))
-  }
-}
-
-private val log = KInlineLogging.logger()
-
 @SpringBootTestWithTestProfile(
   customProperties = [
     "config.preference.pv-preference=NicoNicoDouga, Youtube",
@@ -52,3 +28,5 @@ internal class EnablementConfig2Test(
     enablement[PVServicesEnum.NICONICODOUGA] shouldContainExactlyInAnyOrder listOf("youtube-dl", "other-downloader")
   }
 })
+
+private val log = KInlineLogging.logger()
