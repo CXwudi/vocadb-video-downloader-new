567ee44ca424b926f1e52e175f7b9a02393d210a
CXwudi <cx2010017@gmail.com>
2021-11-10 18:56:26 -0500

    save what we have so far

--- vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/model/Models.kt
+++ vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/model/Models.kt
@@ -1,5 +1,6 @@
 package mikufan.cx.vvd.downloader.model
 
+import mikufan.cx.vocadbapiclient.model.PVContract
 import mikufan.cx.vocadbapiclient.model.SongForApiContract
 import mikufan.cx.vvd.common.label.VSongLabel
 
@@ -12,4 +13,16 @@ data class VSongTask(val label: VSongLabel, val parameters: Parameters)
 
 data class Parameters(
   var songForApiContract: SongForApiContract? = null,
+  /**
+   * the order of try download which PV first, then which next
+   */
+  val pvCandidates: MutableList<PVTask> = mutableListOf(),
 )
+
+/**
+ * using a data class to group related PV infos
+ */
+data class PVTask(
+  val pv: PVContract,
+
+  )
