2d83b48466a83dd7c8a39ae77e15d9ccc92213d2
CXwudi <cx2010017@gmail.com>
2022-01-28 03:12:07 -0500

    disable a logging to avoid double logging problem

--- vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/config/downloader/DownloaderBaseCondition.kt
+++ vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/config/downloader/DownloaderBaseCondition.kt
@@ -1,6 +1,5 @@
 package mikufan.cx.vvd.downloader.config.downloader
 
-import mikufan.cx.inlinelogging.KInlineLogging
 import mikufan.cx.vvd.downloader.util.PVServicesEnum
 import org.springframework.context.annotation.Condition
 import org.springframework.context.annotation.ConditionContext
@@ -26,14 +25,14 @@ sealed interface DownloaderBaseCondition : Condition {
       .contains(pvServices.toString()) &&
         environment.getProperty("config.enablement.$pvServices", "")
           .contains(downloaderName)
-    if (!res) {
-      log.debug { "Skip loading $downloaderName downloader for $pvServices" }
-    }
+    // there will be double logging problem on this code because this base condition is used on both
+    // configuration properties classes and downloader implementation classes.
+//    if (!res) {
+//      log.debug { "Skip loading $downloaderName downloader for $pvServices" }
+//    }
     return res
   }
 
   val pvServices: PVServicesEnum
   val downloaderName: String
 }
-
-private val log = KInlineLogging.logger()
