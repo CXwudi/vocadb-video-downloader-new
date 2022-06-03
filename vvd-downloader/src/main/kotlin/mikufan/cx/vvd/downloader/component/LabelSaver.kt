a8d8fa2561a81ecff99f82665f0c67594ada4cc1
CXwudi <cx2010017@gmail.com>
2022-06-03 00:43:00 -0400

    :sparkles: finally all done for vvd-downloader
    still need manual testing of whole module
    still have some download unimplemented

---
+++ vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/component/LabelSaver.kt
@@ -0,0 +1,57 @@
+package mikufan.cx.vvd.downloader.component
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.Dispatchers
+import mikufan.cx.inlinelogging.KInlineLogging
+import mikufan.cx.vvd.commonkt.batch.AbstractParallelWriter
+import mikufan.cx.vvd.commonkt.batch.RecordErrorWriter
+import mikufan.cx.vvd.downloader.config.IOConfig
+import mikufan.cx.vvd.downloader.model.VSongTask
+import org.jeasy.batch.core.record.Record
+import org.springframework.stereotype.Component
+import kotlin.io.path.absolute
+import kotlin.io.path.deleteExisting
+import kotlin.io.path.div
+import kotlin.io.path.moveTo
+
+/**
+ * Save the -label json and any other files to the output directory.
+ *
+ * Each label json represent one task.
+ * @date 2022-06-02
+ * @author CX无敌
+ */
+@Component
+class LabelSaver(
+  ioConfig: IOConfig,
+  private val objectMapper: ObjectMapper,
+  recordErrorWriter: RecordErrorWriter
+) : AbstractParallelWriter<VSongTask>(recordErrorWriter) {
+
+  private val inputDirectory = ioConfig.inputDirectory
+  private val outputDirectory = ioConfig.outputDirectory
+
+  /**
+   * write a single record to somewhere.
+   *
+   * blocking IO is fine here as this suspend fun is ran in [Dispatchers.IO]
+   */
+  override suspend fun write(record: Record<VSongTask>) {
+    val (label, parameters) = record.payload
+
+    val newLabelFile = outputDirectory / label.labelFileName
+    objectMapper.writeValue(newLabelFile.toFile(), label)
+    inputDirectory.resolve(label.labelFileName).deleteExisting() // the old label can be deleted
+
+    val newInfoFile = outputDirectory / label.infoFileName
+    inputDirectory.resolve(label.infoFileName).moveTo(newInfoFile)
+
+    log.info {
+      "Done all tasks for ${parameters.songProperFileName}. " +
+          "Saved new label json file to ${newLabelFile.absolute()} and deleted the old one from input directory. " +
+          "Moved the song info json file from input directory to ${newInfoFile.absolute()}"
+    }
+  }
+}
+
+private val log = KInlineLogging.logger()
--- vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/service/MainService.kt
+++ vvd-downloader/src/main/kotlin/mikufan/cx/vvd/downloader/service/MainService.kt
@@ -1,8 +1,10 @@
 package mikufan.cx.vvd.downloader.service
 
+import kotlinx.coroutines.runBlocking
 import mikufan.cx.inlinelogging.KInlineLogging
 import mikufan.cx.vvd.commonkt.batch.RecordErrorWriter
 import mikufan.cx.vvd.commonkt.batch.toIterator
+import mikufan.cx.vvd.downloader.component.LabelSaver
 import mikufan.cx.vvd.downloader.component.LabelsReader
 import mikufan.cx.vvd.downloader.model.VSongTask
 import org.jeasy.batch.core.processor.RecordProcessor
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Service
 class MainService(
   private val labelsReader: LabelsReader,
   private val processors: List<RecordProcessor<*, *>>,
+  private val labelSaver: LabelSaver,
   private val errorWriter: RecordErrorWriter
 ) : Runnable {
 
@@ -24,15 +27,18 @@ class MainService(
     for (label in labelsReader.toIterator()) {
       processDownload(label)
     }
+    log.info { "やった！PVのダウンロードを全部完成した。続きは音楽を抽出するに行くぞ o(*￣▽￣*)ブ" }
   }
 
   @Suppress("UNCHECKED_CAST")
+  // current we don't need to cast to Any, but for better robustness, let's keep it for now
   private fun processDownload(label: Record<VSongTask>) {
     var current: Record<Any> = label as Record<Any>
     try {
       for (processor in processors) {
         current = (processor as RecordProcessor<Any, Any>).processRecord(current)
       }
+      runBlocking { labelSaver.write(current as Record<VSongTask>) }
     } catch (e: Exception) {
       errorWriter.handleError(current, e)
     }
