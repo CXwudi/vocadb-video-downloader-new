6999ef97b435c02fc31f61ab09684718e1e8d4a9
CXwudi <cx2010017@gmail.com>
2022-06-12 16:27:26 -0400

    added BeforeProcessLabelValidator.kt

---
+++ vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/component/BeforeProcessLabelValidator.kt
@@ -0,0 +1,26 @@
+package mikufan.cx.vvd.extractor.component
+
+import mikufan.cx.inlinelogging.KInlineLogging
+import mikufan.cx.vvd.common.label.ValidationPhase
+import mikufan.cx.vvd.commonkt.batch.CustomizableBeanRecordValidator
+import mikufan.cx.vvd.extractor.model.VSongTask
+import mikufan.cx.vvd.extractor.util.OrderConstants
+import org.jeasy.batch.core.record.Record
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import javax.validation.Validator
+
+@Component
+@Order(OrderConstants.BEFORE_PROCESS_LABEL_VALIDATOR_ORDER)
+class BeforeProcessLabelValidator(
+  validator: Validator
+) : CustomizableBeanRecordValidator<VSongTask>(validator, ValidationPhase.Two::class.java) {
+  override fun processRecord(record: Record<VSongTask>): Record<VSongTask> {
+    log.info { "Validate label info before processing: ${record.payload.label.labelFileName}" }
+    return super.processRecord(record)
+  }
+
+  override fun Record<VSongTask>.toValidationObject() = this.payload.label
+}
+
+private val log = KInlineLogging.logger()
