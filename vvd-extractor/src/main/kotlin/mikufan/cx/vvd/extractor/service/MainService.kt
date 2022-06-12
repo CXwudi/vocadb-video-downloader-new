90888d4b59fe59540f52ae99cd0545e673963d68
CXwudi <cx2010017@gmail.com>
2022-06-12 16:27:54 -0400

    :sparkles: done the processing part of MainService.kt,

--- vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/service/MainService.kt
+++ vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/service/MainService.kt
@@ -1,9 +1,19 @@
 package mikufan.cx.vvd.extractor.service
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Semaphore
 import mikufan.cx.inlinelogging.KInlineLogging
+import mikufan.cx.vvd.commonkt.batch.RecordErrorWriter
+import mikufan.cx.vvd.commonkt.batch.toIterator
 import mikufan.cx.vvd.extractor.component.LabelsReader
+import mikufan.cx.vvd.extractor.config.BatchConfig
+import mikufan.cx.vvd.extractor.model.VSongTask
+import org.jeasy.batch.core.processor.RecordProcessor
+import org.jeasy.batch.core.record.Record
 import org.springframework.stereotype.Service
+import java.util.*
 
 /**
  * @date 2022-06-11
@@ -11,11 +21,46 @@ import org.springframework.stereotype.Service
  */
 @Service
 class MainService(
-  private val labelsReader: LabelsReader
+  private val labelsReader: LabelsReader,
+  private val taskProcessors: List<RecordProcessor<*, *>>,
+  private val recordErrorWriter: RecordErrorWriter,
+  batchConfig: BatchConfig
 ) : Runnable {
-  
-  override fun run() = runBlocking {
 
+  private val semaphore = Semaphore(batchConfig.batchSize)
+
+  override fun run() {
+    // this map can potentially be large as it eventually saves all song info and label info
+    val processed = Collections.synchronizedMap(mutableMapOf<Int, VSongTask>())
+    runBlocking(Dispatchers.IO) {
+      semaphore.acquire()
+      labelsReader.toIterator().withIndex().forEach { (index, label) ->
+        launch {
+          processExtract(label).onSuccess {
+            processed[index] = it.payload
+          }
+        }
+        semaphore.acquire()
+      }
+    }
+    TODO("new function to save all data sequentially, with respect to their orders")
+    log.info { "やった！全部完成" }
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  private suspend fun processExtract(label: Record<VSongTask>): Result<Record<VSongTask>> {
+    var currentRecord: Record<Any> = label as Record<Any>
+    return try {
+      taskProcessors.forEach { recordProcessor ->
+        currentRecord = (recordProcessor as RecordProcessor<Any, Any>).processRecord(currentRecord)
+      }
+      Result.success(currentRecord as Record<VSongTask>)
+    } catch (e: Exception) {
+      recordErrorWriter.handleError(currentRecord, e)
+      Result.failure(e)
+    } finally {
+      semaphore.release() // release the semaphore when done to allow next task continues
+    }
   }
 }
 
