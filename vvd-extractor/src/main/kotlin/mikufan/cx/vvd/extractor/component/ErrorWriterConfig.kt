26f26770f9e7362086c6bb27c25e6a47fb6e2be5
CXwudi <cx2010017@gmail.com>
2022-06-12 15:06:43 -0400

    :rocket: ported OrderConstants.kt and ErrorWriterConfig.kt

---
+++ vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/component/ErrorWriterConfig.kt
@@ -0,0 +1,28 @@
+package mikufan.cx.vvd.extractor.component
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import mikufan.cx.vvd.commonkt.batch.RecordErrorWriter
+import mikufan.cx.vvd.commonkt.naming.toErrorFileName
+import mikufan.cx.vvd.extractor.config.IOConfig
+import mikufan.cx.vvd.extractor.model.VSongTask
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/**
+ * @date 2021-09-28
+ * @author CX无敌
+ */
+@Configuration
+class ErrorWriterConfig {
+
+  @Bean
+  fun errorWriter(ioConfig: IOConfig, objectMapper: ObjectMapper) =
+    RecordErrorWriter(ioConfig.errorDirectory, objectMapper) {
+      val payload = it.payload
+      if (payload is VSongTask) {
+        payload.parameters.songForApiContract?.toErrorFileName() ?: "unknown song ${it.header}.json"
+      } else {
+        ""
+      }
+    }
+}
---
+++ vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/util/OrderConstants.kt
@@ -0,0 +1,6 @@
+package mikufan.cx.vvd.extractor.util
+
+object OrderConstants {
+  const val BEFORE_PROCESS_LABEL_VALIDATOR_ORDER = 1
+  const val SONG_INFO_LOADER_ORDER = 2
+}
