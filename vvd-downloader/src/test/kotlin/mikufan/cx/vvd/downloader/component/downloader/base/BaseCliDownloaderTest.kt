d2a8bcdbe9a36317a18ded861f3d79211c59393e
CXwudi <cx2010017@gmail.com>
2022-07-15 19:08:02 -0400

    refactor

--- vvd-downloader/src/test/kotlin/mikufan/cx/vvd/downloader/component/downloader/base/BaseCliDownloaderTest.kt
+++ vvd-downloader/src/test/kotlin/mikufan/cx/vvd/downloader/component/downloader/base/BaseCliDownloaderTest.kt
@@ -1,10 +1,9 @@
-package mikufan.cx.vvd.downloader.component.downloader
+package mikufan.cx.vvd.downloader.component.downloader.base
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.kotest.assertions.fail
 import io.kotest.matchers.string.shouldContain
 import mikufan.cx.vvd.commonkt.vocadb.PVServicesEnum
-import mikufan.cx.vvd.downloader.component.downloader.base.BaseCliDownloader
 import mikufan.cx.vvd.downloader.config.DownloadConfig
 import mikufan.cx.vvd.downloader.config.EnvironmentConfig
 import mikufan.cx.vvd.downloader.config.IOConfig
