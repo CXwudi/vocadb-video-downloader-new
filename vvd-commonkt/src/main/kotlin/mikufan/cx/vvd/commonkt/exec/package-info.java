fc8d89dc813083cfde79f3d48ae0b4bcd2519a6f
CXwudi <cx2010017@gmail.com>
2022-07-15 17:21:20 -0400

    :heavy_minus_sign: remove unused code

--- vvd-commonkt/src/main/kotlin/mikufan/cx/vvd/commonkt/exec/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * embedded the https://github.com/CXwudi/kotlin-jvm-idiomatic-exec source code until we fix the "java.io.IOException: Stream closed" issue
- * which causes jvm program not exiting
- * @date 2022-06-11
- * @author CX无敌
- */
-@Deprecated // this is only for testing propose, we don't want to use this in production
-package mikufan.cx.vvd.commonkt.exec;
