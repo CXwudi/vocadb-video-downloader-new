f5c522541b983cbbf577c1c875c7f6ec3d29edf5
CXwudi <cx2010017@gmail.com>
2022-07-19 22:57:21 -0400

    :memo: add doc

--- vvd-extractor/src/test/kotlin/mikufan/cx/vvd/extractor/service/MainServiceTest.kt
+++ vvd-extractor/src/test/kotlin/mikufan/cx/vvd/extractor/service/MainServiceTest.kt
@@ -4,9 +4,13 @@ import io.kotest.assertions.throwables.shouldNotThrow
 import mikufan.cx.vvd.extractor.util.SpringBootDirtyTestWithTestProfile
 import mikufan.cx.vvd.extractor.util.SpringShouldSpec
 
+/**
+ * only this vvd-extractor module can have this integration test running in CI without fare
+ *
+ * if running in local environment, be aware that it will move files, and don't commit that changes
+ */
 @SpringBootDirtyTestWithTestProfile(
   customProperties = [
-
   ]
 )
 class MainServiceTest(
