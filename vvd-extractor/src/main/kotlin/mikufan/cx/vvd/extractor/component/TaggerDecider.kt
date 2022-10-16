85d591eef9c6657d9a690f29bf6317df9dbd81e2
CXwudi <cx2010017@gmail.com>
2022-10-16 15:59:15 -0400

    :sparkles: completed TaggerDecider

--- vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/component/TaggerDecider.kt
+++ vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/component/TaggerDecider.kt
@@ -3,10 +3,13 @@ package mikufan.cx.vvd.extractor.component
 import mikufan.cx.vvd.common.exception.RuntimeVocaloidException
 import mikufan.cx.vvd.extractor.component.extractor.impl.AacToM4aAudioExtractor
 import mikufan.cx.vvd.extractor.component.extractor.impl.OpusToOggAudioExtractor
+import mikufan.cx.vvd.extractor.component.tagger.impl.M4aAudioTagger
+import mikufan.cx.vvd.extractor.component.tagger.impl.OggOpusAudioTagger
 import mikufan.cx.vvd.extractor.model.VSongTask
 import mikufan.cx.vvd.extractor.util.OrderConstants
 import org.jeasy.batch.core.processor.RecordProcessor
 import org.jeasy.batch.core.record.Record
+import org.springframework.beans.factory.getBean
 import org.springframework.context.ApplicationContext
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
@@ -28,17 +31,17 @@ class TaggerDecider(
     if (decidedExtractorOpt.isPresent) {
       // the format of the audio file produced by audio extractor is pretty much fixed, so we can simply map it to correct tagger
       when (val decidedExtractor = decidedExtractorOpt.get()) {
-        is AacToM4aAudioExtractor -> TODO()
-        is OpusToOggAudioExtractor -> TODO()
+        is AacToM4aAudioExtractor -> ctx.getBean<M4aAudioTagger>()
+        is OpusToOggAudioExtractor -> ctx.getBean<OggOpusAudioTagger>()
         else -> throw IllegalStateException("This should not happened, unknown audio extractor: $decidedExtractor")
       }
     } else {
       // no audio extractor = using audio file copied from label to outputDirectory
       val audioFile = requireNotNull(parameters.processedAudioFile) { "null processedAudioFile?" }
       when (val audioFormat = audioMediaFormatChecker.checkAudioFormat(audioFile)) {
-        "aac" -> TODO()
-        "opus" -> TODO()
-        else -> throw RuntimeVocaloidException("Audio format: $audioFormat is not supported")
+        "aac" -> ctx.getBean<M4aAudioTagger>()
+        "opus" -> ctx.getBean<OggOpusAudioTagger>()
+        else -> throw RuntimeVocaloidException("Audio format $audioFormat is not supported from $audioFile")
       }
     }
     return record
