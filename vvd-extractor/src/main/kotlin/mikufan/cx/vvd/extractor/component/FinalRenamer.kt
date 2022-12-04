d7b97c34b2e8e6905ba5fcad397447af269bb397
CXwudi <cx2010017@gmail.com>
2022-12-03 22:47:51 -0500

    :memo: need test cases for FinalRenamer

--- vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/component/FinalRenamer.kt
+++ vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/component/FinalRenamer.kt
@@ -2,7 +2,9 @@ package mikufan.cx.vvd.extractor.component
 
 import mikufan.cx.inlinelogging.KInlineLogging
 import mikufan.cx.vocadbapiclient.model.SongForApiContract
+import mikufan.cx.vvd.commonkt.naming.SongProperFileName
 import mikufan.cx.vvd.commonkt.naming.removeIllegalChars
+import mikufan.cx.vvd.commonkt.naming.renameWithSameExtension
 import mikufan.cx.vvd.extractor.model.VSongTask
 import mikufan.cx.vvd.extractor.util.OrderConstants
 import org.jeasy.batch.core.processor.RecordProcessor
@@ -10,7 +12,6 @@ import org.jeasy.batch.core.record.Record
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import java.nio.file.Path
-import kotlin.io.path.extension
 
 /**
  * @author CX无敌
@@ -41,25 +42,25 @@ class FinalRenamerCore {
     songInfo: SongForApiContract,
     vocaDbPvId: Int,
   ): Path {
-    val newFileName = "${generateProperName(songInfo, vocaDbPvId)}.${audioFile.extension}"
-    val newFile = audioFile.resolveSibling(newFileName)
-    audioFile.toFile().renameTo(newFile.toFile())
-    log.info { "Renamed" }
+    val newFileName = generateProperName(songInfo, vocaDbPvId)
+    val newFile = audioFile.renameWithSameExtension(newFileName.toString())
+    log.info { "Rename to proper filename $newFileName" }
     return newFile
   }
 
   internal fun generateProperName(
     songInfo: SongForApiContract,
     vocaDbPvId: Int,
-  ): String {
+  ): SongProperFileName {
     val artists: List<String> = requireNotNull(songInfo.artistString) { "artist string is null" }.split("feat.")
     val vocals = artists[1].trim()
     val producers = artists[0].trim()
-    val songName: String = requireNotNull(songInfo.defaultName) { "song name is null" }
+    val songName = requireNotNull(songInfo.defaultName) { "song name is null" }
     val pvInfo = requireNotNull(songInfo.pvs?.find { it.id == vocaDbPvId }) { "pv info is null" }
-    val pvService = requireNotNull(pvInfo.service?.name) { "pv service is null" }
+    val pvService = requireNotNull(pvInfo.service) { "pv service is null" }
     val pvId = requireNotNull(pvInfo.pvId) { "pv id is null" }
-    return removeIllegalChars("$songName - $vocals feat. $producers [$pvService $pvId]")
+    val fileName = "【%s】%s【%s】[%s %s]".format(vocals, songName, producers, pvService, pvId)
+    return SongProperFileName(removeIllegalChars(fileName))
   }
 }
 
---
+++ vvd-extractor/src/test/kotlin/mikufan/cx/vvd/extractor/component/FinalRenamerTest.kt
@@ -0,0 +1,14 @@
+package mikufan.cx.vvd.extractor.component
+
+import io.kotest.core.spec.style.ShouldSpec
+
+class FinalRenamerTest : ShouldSpec({
+
+  context("proper file name") {
+    // TODO
+  }
+
+  context("file renaming") {
+    // TODO
+  }
+})
