d6bacfc28e15642619fdaf64cbe9d675ae0ecef6
CXwudi <cx2010017@gmail.com>
2022-12-26 11:53:47 -0500

    :white_check_mark: finished FinalRenamerTest

--- vvd-extractor/src/test/kotlin/mikufan/cx/vvd/extractor/component/FinalRenamerTest.kt
+++ vvd-extractor/src/test/kotlin/mikufan/cx/vvd/extractor/component/FinalRenamerTest.kt
@@ -1,14 +1,36 @@
 package mikufan.cx.vvd.extractor.component
 
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import mikufan.cx.vocadbapiclient.model.PVContract
+import mikufan.cx.vocadbapiclient.model.PVService
+import mikufan.cx.vocadbapiclient.model.SongForApiContract
+import kotlin.io.path.deleteIfExists
 
 class FinalRenamerTest : ShouldSpec({
 
+  val fileRenamer = FinalRenamerCore()
+  val vocadbPvId = 39
+  val testSong = SongForApiContract().apply {
+    defaultName = "Test Song with /"
+    artistString = "Producer feat. Vocalist"
+    pvs = listOf(
+      PVContract().apply {
+        id = vocadbPvId
+        pvId = "sm123456"
+        service = PVService.NICONICODOUGA
+      }
+    )
+  }
   context("proper file name") {
-    // TODO
+    fileRenamer.generateProperName(testSong, vocadbPvId)
+      .toString() shouldBe "【Vocalist】Test Song with -【Producer】[NicoNicoDouga sm123456]"
   }
 
   context("file renaming") {
-    // TODO
+    val tempFile = kotlin.io.path.createTempFile("test-", ".temp.txt")
+    val path = fileRenamer.doProperRename(tempFile, testSong, vocadbPvId)
+    path.fileName.toString() shouldBe "【Vocalist】Test Song with -【Producer】[NicoNicoDouga sm123456].txt"
+    path.deleteIfExists()
   }
 })
