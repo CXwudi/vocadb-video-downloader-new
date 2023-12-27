dc2bc9472d9a80cebf05b8a2534052bdc600dc5a
CXwudi <cx2010017@gmail.com>
2023-12-27 16:02:04 -0500

    :wrench: make const

--- vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/util/AudioMediaFormat.kt
+++ vvd-extractor/src/main/kotlin/mikufan/cx/vvd/extractor/util/AudioMediaFormat.kt
@@ -1,7 +1,7 @@
 package mikufan.cx.vvd.extractor.util
 
 object AudioMediaFormat {
-  val AAC = "aac"
-  val OPUS = "opus"
-  val MPEG_AUDIO = "mpeg audio"
+  const val AAC = "aac"
+  const val OPUS = "opus"
+  const val MPEG_AUDIO = "mpeg audio"
 }
