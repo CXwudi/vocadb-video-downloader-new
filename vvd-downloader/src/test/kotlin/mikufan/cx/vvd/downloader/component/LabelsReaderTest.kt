66ebafbd52d37a00bebb13be54ed18f1168b9ca8
CXwudi <cx2010017@gmail.com>
2026-02-23 19:55:20 -0500

    🧪 test: use long counter for label order

--- vvd-downloader/src/test/kotlin/mikufan/cx/vvd/downloader/component/LabelsReaderTest.kt
+++ vvd-downloader/src/test/kotlin/mikufan/cx/vvd/downloader/component/LabelsReaderTest.kt
@@ -10,15 +10,15 @@ class LabelsReaderTest(
 ) {
   @Test
   fun worksOnNormalInputFromTaskProducer() {
-    var count = 0
+    var count = 0L
     do {
       val record = labelsReader.readRecord()
-      count += if (record != null) 1 else 0
+      count += if (record != null) 1L else 0L
       if (record != null) {
         assertThat(count).isEqualTo(record.payload.label.order)
       }
     } while (record != null)
     // our own sample input directory have 154 labels
-    assertThat(count).isEqualTo(154) // WARNING: update the count when changed to new list
+    assertThat(count).isEqualTo(154L) // WARNING: update the count when changed to new list
   }
 }
